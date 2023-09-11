7acf3ea63db2851220b5003a08fbe88a55b1e065
Suin <tktw555@gmail.com>
2023-09-11 17:48:49 +0900

    bidHistoryChart.html 그래프 날짜별로 쪼개기

--- src/main/java/com/foot/service/BidHistoryService.java
+++ src/main/java/com/foot/service/BidHistoryService.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -17,12 +20,43 @@ import java.util.List;
 public class BidHistoryService {
 
     private final BidHistoryRepository bidHistoryRepository;
+//    public List<BidHistoryChartData> getChartDataForLastWeek() {
+//        LocalDateTime endDate = LocalDateTime.now();  // 현재 날짜
+//        LocalDateTime startDate = endDate.minusWeeks(1);  // 오늘로부터 일주일 전의 날짜 계산
+//        // Spring Data JPA 쿼리 메서드 호출
+//        return bidHistoryRepository.getChartDataByDate(startDate, endDate);
+//    }
+
     public List<BidHistoryChartData> getChartDataForLastWeek() {
         LocalDateTime endDate = LocalDateTime.now();  // 현재 날짜
         LocalDateTime startDate = endDate.minusWeeks(1);  // 오늘로부터 일주일 전의 날짜 계산
 
         // Spring Data JPA 쿼리 메서드 호출
-        return bidHistoryRepository.getChartDataByDate(startDate, endDate);
+        List<BidHistoryChartData> chartDataList = bidHistoryRepository.getChartDataByDate(startDate, endDate);
+
+        // 일별로 그룹화된 데이터를 생성
+        List<BidHistoryChartData> dailyChartDataList = groupChartDataByDay(chartDataList);
+
+        return dailyChartDataList;
     }
+
+    private List<BidHistoryChartData> groupChartDataByDay(List<BidHistoryChartData> chartDataList) {
+        Map<LocalDate, Long> dailyChartDataMap = new LinkedHashMap<>();
+
+        for (BidHistoryChartData data : chartDataList) {
+            LocalDateTime createdAt = data.getDate();
+            LocalDate date = createdAt.toLocalDate();
+            long count = data.getCount();
+
+            // 이미 해당 날짜에 데이터가 있는 경우 더하고, 없으면 새로 추가
+            dailyChartDataMap.merge(date, count, Long::sum);
+        }
+
+        // Map을 다시 BidHistoryChartData 리스트로 변환
+        return dailyChartDataMap.entrySet().stream()
+                .map(entry -> new BidHistoryChartData(entry.getKey().atStartOfDay(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+
 
 }
